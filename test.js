3de02a2f289812267feffcfc2c753a58bf99a25c
TarasLys <opros0609@gmail.com>
2023-11-03 09:24:37 +0200

    www

---
+++ test.js
@@ -0,0 +1,108 @@
+// const fetchPostsBtn = document.querySelector(".btn");
+// const postList = document.querySelector(".posts");
+
+// // Controls the group number
+// let page = 1;
+// // Controls the number of items in the group
+// let perPage = 10;
+
+// fetchPostsBtn.addEventListener("click", () => {
+//   fetchPosts()
+//     .then((posts) => {
+//       renderPosts(posts);
+//       // Increase the group number
+//       page += 1;
+
+//       // Replace button text after first request
+//       if (page > 1) {
+//         fetchPostsBtn.textContent = "Fetch more posts";
+//       }
+//     })
+//     .catch((error) => console.log(error));
+// });
+
+// function fetchPosts() {
+//   const params = new URLSearchParams({
+//     _limit: perPage,
+//     _page: page
+//   });
+
+//   return fetch(`https://jsonplaceholder.typicode.com/posts?${params}`).then(
+//     (response) => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     }
+//   );
+// }
+
+// function renderPosts(posts) {
+//   const markup = posts
+//     .map(({ id, title, body, userId }) => {
+//       return `<li>
+//           <h2 class="post-title">${title.slice(0, 30)}</h2>
+//           <p><b>Post id</b>: ${id}</p>
+//           <p><b>Author id</b>: ${userId}</p>
+//           <p class="post-body">${body}</p>
+//         </li>`;
+//     })
+//     .join("");
+//   postList.insertAdjacentHTML("beforeend", markup);
+// }
+
+
+const icon = document.querySelector(".icon")
+const formRequest = document.querySelector(".form-request")
+
+const txtItem = document.querySelector(".textarea__item")
+const txtItemLimit = txtItem.getAttribute("maxlength")
+const txtCounter = document.querySelector(".textarea__counter span")
+
+
+formRequest.style.display = "none"
+
+icon.addEventListener("click", openForm)
+
+function openForm(e) {
+e.stopPropagation()
+
+  const opfo = e.target;
+  if (opfo) {
+    
+   return formRequest.style.display = "block"
+  } 
+}
+
+document.addEventListener("keyup", closeForm)
+function closeForm(e) {
+  //console.dir(e.code)
+  if (e.code === "Escape") { 
+    formRequest.style.display = "none"
+  }
+
+}
+
+document.addEventListener("click", offClick)
+
+function offClick(e) {
+  
+  //console.log(clfo)
+  if (e.target && !e.target.closest(".form-request")) {
+  formRequest.style.display = "none"
+  } 
+}
+
+txtCounter.innerHTML = txtItemLimit
+
+txtItem.addEventListener("keyup", listSymbol)
+function listSymbol(e) {
+  const result = txtItemLimit - e.target.value.length
+  //console.log(result)
+  txtCounter.innerHTML = result;
+}
+
+txtItem.addEventListener(".keydown", repeatSymb)
+function repeatSymb(e) {
+  if (e.repeat) listSymbol() 
+}
